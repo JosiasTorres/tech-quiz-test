664adf3ee6fe4a606b2c054cb0107fae79cf664e
Josias Torres <josiastorresvillegas@Josiass-MacBook-Pro.local>
2025-03-17 23:48:56 -0600

    🔥 Eliminado tsconfig.node.json innecesario en client/

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Quiz from '../../client/src/components/Quiz';
-import { mount } from 'cypress/react';
+import { mount } from 'cypress/react18';
 
 describe('Quiz Component', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/questions/random', {
+      statusCode: 200,
+      body: [
+        { question: 'What is 2 + 2?', options: ['3', '4', '5'], correctAnswer: '4' },
+        { question: 'What is the capital of France?', options: ['Berlin', 'Madrid', 'Paris'], correctAnswer: 'Paris' }
+      ]
+    }).as('getQuestions');
+  });
+
   it('Debe renderizar correctamente el componente Quiz', () => {
     mount(<Quiz />);
     cy.contains('Start Quiz').should('be.visible');
@@ -11,12 +21,14 @@ describe('Quiz Component', () => {
   it('Debe iniciar el quiz al hacer clic en "Start Quiz"', () => {
     mount(<Quiz />);
     cy.contains('Start Quiz').click();
+    cy.wait('@getQuestions'); // Esperar la respuesta simulada
     cy.get('.question-container').should('be.visible');
   });
 
   it('Debe avanzar a la siguiente pregunta al seleccionar una respuesta', () => {
     mount(<Quiz />);
     cy.contains('Start Quiz').click();
+    cy.wait('@getQuestions');
     cy.get('.answer-option').first().click();
     cy.get('.question-container').should('exist');
   });
@@ -24,6 +36,7 @@ describe('Quiz Component', () => {
   it('Debe mostrar el puntaje final al terminar el quiz', () => {
     mount(<Quiz />);
     cy.contains('Start Quiz').click();
+    cy.wait('@getQuestions');
     cy.get('.answer-option').each(($el) => {
       cy.wrap($el).click();
     });
